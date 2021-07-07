35677d76c4d8472e9baedf86d23e35e7f5a68812
Konstantin <zkonstr@gmail.com>
2021-07-07 18:42:33 +0300

    Image filter added

---
+++ Filter/ImageFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace PhotoFilter.Filter
+{
+    public class ImageFilter
+    {
+        public Bitmap Image { get; set; }
+
+        public ImageFilter(Bitmap image)
+        {
+            Image = image;
+        }
+
+        public Bitmap Filter(int radius)
+        {
+            var pixelMatrix = ImageConverter.ToRgbMatrix(Image);
+            MatrixFilter filter = new MatrixFilter(pixelMatrix);
+            var filtered = filter.Filter(radius);
+            return ImageConverter.FromRgbMatrix(filtered);
+        }
+    }
+}
