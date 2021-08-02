888a72dc1eabdf4e0d368684a5d06f49a78946ae
Konstantin <zkonstr@gmail.com>
2021-08-02 17:20:25 +0300

    Hsv converter added

--- Filter/ImageConverter.cs
+++ Filter/ImageConverter.cs
@@ -1,21 +1,41 @@
 ﻿using System.Drawing;
+using PhotoFilter.Intern;
 
 namespace PhotoFilter.Filter
 {
     public static class ImageConverter
     {
-        public static int[,] ToRgbMatrix(Bitmap bpm)
+        public static int[,] ToRgbMatrix(Bitmap bmp)
         {
-            var imageMatrix = new int[bpm.Height, bpm.Width];
-            for (int i = 0; i < bpm.Height; i++)
+            var imageMatrix = new int[bmp.Height, bmp.Width];
+            for (int i = 0; i < bmp.Height; i++)
             {
-                for (int j = 0; j < bpm.Width; j++)
+                for (int j = 0; j < bmp.Width; j++)
                 {
-                    imageMatrix[i, j] = bpm.GetPixel(j, i).ToArgb();
+                    imageMatrix[i, j] = bmp.GetPixel(j, i).ToArgb();
                 }
             }
 
             return imageMatrix;
+        }
+
+        public static float[][,] ToHsvMatrices(Bitmap bmp)
+        {
+            var hMatrix = new float[bmp.Height, bmp.Width];
+            var sMatrix = new float[bmp.Height, bmp.Width];
+            var vMatrix = new float[bmp.Height, bmp.Width];
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    var color = bmp.GetPixel(j, i);
+                    hMatrix[i, j] = color.GetHue();
+                    sMatrix[i, j] = color.GetSaturation();
+                    vMatrix[i, j] = color.GetBrightness();
+                }
+            }
+
+            return new float[][,] {hMatrix, sMatrix, vMatrix};
         }
 
         public static Bitmap FromRgbMatrix(int[,] imageMatrix)
@@ -25,11 +45,22 @@ namespace PhotoFilter.Filter
             {
                 for (int j = 0; j < bmp.Width; j++)
                 {
-                    imageMatrix[i, j] = bmp.GetPixel(j, i).ToArgb();
                     bmp.SetPixel(j, i, Color.FromArgb(imageMatrix[i, j]));
                 }
             }
+            return bmp;
+        }
 
+        public static Bitmap FromHsvMatrices(float[,] hMatrix, float[,] sMatrix, float[,] vMatrix)
+        {
+            Bitmap bmp = new Bitmap(hMatrix.GetLength(1), hMatrix.GetLength(0));
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    bmp.SetPixel(j, i, HsvConverter.ToRgb(hMatrix[i, j], sMatrix[i, j], vMatrix[i, j]));
+                }
+            }
             return bmp;
         }
     }
---
+++ Intern/HsvConverter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+
+namespace PhotoFilter.Intern
+{
+    public class HsvConverter
+    {
+        public static Color ToRgb(float h, float s, float v)
+        {
+            double r = 0, g = 0, b = 0;
+
+            if (s == 0)
+            {
+                r = v;
+                g = v;
+                b = v;
+            }
+            else
+            {
+                int i;
+                double f, p, q, t;
+
+                if (h == 360)
+                    h = 0;
+                else
+                    h = h / 60;
+
+                i = (int) Math.Truncate(h);
+                f = h - i;
+
+                p = v * (1.0 - s);
+                q = v * (1.0 - (s * f));
+                t = v * (1.0 - (s * (1.0 - f)));
+
+                switch (i)
+                {
+                    case 0:
+                        r = v;
+                        g = t;
+                        b = p;
+                        break;
+
+                    case 1:
+                        r = q;
+                        g = v;
+                        b = p;
+                        break;
+
+                    case 2:
+                        r = p;
+                        g = v;
+                        b = t;
+                        break;
+
+                    case 3:
+                        r = p;
+                        g = q;
+                        b = v;
+                        break;
+
+                    case 4:
+                        r = t;
+                        g = p;
+                        b = v;
+                        break;
+
+                    default:
+                        r = v;
+                        g = p;
+                        b = q;
+                        break;
+                }
+            }
+
+            return Color.FromArgb((int) (r * 255), (int) (g * 255), (int) (b * 255));
+        }
+    }
+}
