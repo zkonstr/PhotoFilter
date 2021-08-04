b112b2d84fa828c56ca441bb74ef822335777e93
Konstantin <zkonstr@gmail.com>
2021-08-04 20:22:34 +0300

    Feat: multithreading added

--- Filter/ImageFilterParallel.cs
+++ Filter/ImageFilterParallel.cs
@@ -2,22 +2,22 @@
 
 namespace PhotoFilter.Filter
 {
-    public class ImageFilter
+    public class ImageFilterParallel
     {
-        public ImageFilter(Bitmap image)
+        public ImageFilterParallel(Bitmap image)
         {
             Image = image;
         }
 
         public Bitmap Image { get; set; }
 
-        public Bitmap Filter(int radius)
+        public Bitmap Filter(int radius, int threadsCount)
         {
             var pixelMatrices = ImageConverter.ToHsvMatrices(Image);
             var filteredMatrices = new float[3][,];
             for (var i = 0; i < pixelMatrices.Length; i++)
             {
-                var filter = new MatrixFilter(pixelMatrices[i]);
+                var filter = new MatrixFilterParallel(pixelMatrices[i], threadsCount);
                 filteredMatrices[i] = filter.Filter(radius);
             }
 
